9d58898dfe337257423acdf16648daa3f2e1722e
James Fuller <fullerjamesr@gmail.com>
2020-09-23 11:34:22 -0500

    Big changes: Re-factored exit status code, improved read-ability, moved body to main() function

--- scripts/mvf_progress_watcher.py
+++ scripts/mvf_progress_watcher.py
@@ -12,6 +12,7 @@ import queue
 import threading
 import mrcfile
 from PIL import Image
+import atexit
 
 
 def explode_path(path):
@@ -25,7 +26,7 @@ def explode_path(path):
 
     Parameters
     ----------
-    path : str
+    path : str or os.PathLike
 
     Returns
     -------
@@ -45,7 +46,20 @@ def explode_path(path):
     return allparts
 
 
-def worker(q):
+def threaded_worker(q):
+    """
+    This function will consume and execute the contents of a threading.Queue object until it is empty.
+
+    Expected items in the queue are tuples of (function, primary argument, **kwargs)
+
+    Parameters
+    ----------
+    q : threading.Queue
+
+    Returns
+    -------
+    None
+    """
     while True:
         try:
             func, fn, kwargs = q.get(block=False)
@@ -55,7 +69,25 @@ def worker(q):
             break
 
 
-def mrc2png(input_file, output_dir=None, resize=None, sigma_contrast=False):
+def mrc2png(input_file, output_dir=None, resize=0, sigma_contrast=0.0):
+    """
+    Convert a .mrc file to a .png image. The output filename will be `input_file` with the .png extension appended.
+
+    Parameters
+    ----------
+    input_file : str or os.PathLike
+        The path to a .mrc file to read and convert
+    output_dir : str or os.PathLike, optional
+        The path to a directory in which to save the file
+    resize : int, optional
+        Resize the output so that it has this width (in pixels)
+    sigma_contrast : float, optional
+        Transform the .mrc data to this sigma contrast before PNG encoding
+
+    Returns
+    -------
+    None
+    """
     if output_dir:
         filename = os.path.split(input_file)[-1]
         output = os.path.join(output_dir, filename) + ".png"
@@ -81,6 +113,22 @@ def mrc2png(input_file, output_dir=None, resize=None, sigma_contrast=False):
 
 
 def ctf2png(input_file, output_dir=None, size=None):
+    """
+    Wrapper around the ctffind_plot_results_png.sh script, used to emit the familiar CTFFind results plots to .png file
+
+    Parameters
+    ----------
+    input_file : str or os.PathLike
+        The path to a CTFFind output _avrot.txt file, the required argument to the ctffind_plot_results script
+    output_dir : str or os.PathLike, optional
+        The path to a directory in which to save the .png file
+    size : int, optional
+        Resize the output so that it has this width (in pixels)
+
+    Returns
+    -------
+    None
+    """
     if output_dir:
         filename = os.path.split(input_file)[-1]
         output = os.path.join(output_dir, filename) + ".png"
@@ -92,106 +140,184 @@ def ctf2png(input_file, output_dir=None, size=None):
         sysrun(['ctffind_plot_results_png.sh', input_file, output], stdout=DEVNULL)
 
 
-###
-# Relion will call this script like:
-# $0 --o External/jobXXX/ --in_YYY ZZZ --LABELN VALUEN --j J
-parser = argparse.ArgumentParser(description="Consolidate MotionCor and CTFFind job results into a single SSV",
-                                 epilog="https://github.com/fullerjamesr/mvf")
-parser.add_argument("--o", required=True)
-parser.add_argument("--in_mics", required=True)
-parser.add_argument("--j", type=int, default=1)
-parser.add_argument("--mic_png_size", type=int, default=1448)
-parser.add_argument("--fft_png_size", type=int, default=None)
-parser.add_argument("--ctf_png_size", type=int, default=None)
-parser.add_argument("--mic_sigma_contrast", type=float, default=2.0)
-args = parser.parse_args()
+def touch_file(file_path):
+    """
+    Mimic the Unix `touch` command: Create the specified file if it doesn't exist, then update its access time
 
-# Before doing any work:
-# (1) Remove any previous signal files (RELION_JOB_EXIT_*)
-# (2) Set up sys.excepthook to touch a RELION_JOB_EXIT_FAILURE file if an uncaught exception kills this script
-RELION_JOB_EXIT_SUCCESS = os.path.join(args.o, 'RELION_JOB_EXIT_SUCCESS')
-RELION_JOB_EXIT_FAILURE = os.path.join(args.o, 'RELION_JOB_EXIT_FAILURE')
-try:
-    os.remove(RELION_JOB_EXIT_SUCCESS)
-    os.remove(RELION_JOB_EXIT_FAILURE)
-except FileNotFoundError:
-    pass
-default_sys_excepthook = sys.excepthook
-def signal_failure(*args):
-    with open(RELION_JOB_EXIT_FAILURE, 'w') as fh:
+    Parameters
+    ----------
+    file_path : str or os.PathLike
+
+    Returns
+    -------
+    None
+    """
+    with open(file_path, 'a'):
+        try:
+            os.utime(file_path, None)
+        except OSError:
+            pass  # File deleted between open() and os.utime() calls
+
+
+def normal_exit(job_dir):
+    """
+    Indicate to Relion that the job has ended normally
+
+    Parameters
+    ----------
+    job_dir : str or os.PathLike
+
+    Returns
+    -------
+    None
+    """
+    touch_file(os.path.join(job_dir, 'RELION_JOB_EXIT_SUCCESS'))
+
+
+def fail_exit(job_dir):
+    """
+    Indicate to Relion that the job has ended with an unexpected error
+
+    Parameters
+    ----------
+    job_dir : str or os.PathLike
+
+    Returns
+    -------
+    None
+    """
+    touch_file(os.path.join(job_dir, 'RELION_JOB_EXIT_FAILURE'))
+
+
+def signal_failure_factory(job_dir):
+    def signal_failure(*args):
+        atexit.unregister(normal_exit)
+        atexit.register(fail_exit, job_dir)
+        sys.__excepthook__(*args)
+    return signal_failure
+
+
+def clear_prior_exits(job_dir):
+    """
+    Cleanup any indicators of previous Relion exit statuses in `job_dir`
+
+    Parameters
+    ----------
+    job_dir : str or os.PathLike
+
+    Returns
+    -------
+    None
+    """
+    try:
+        os.remove(os.path.join(job_dir, 'RELION_JOB_EXIT_SUCCESS'))
+        os.remove(os.path.join(job_dir, 'RELION_JOB_EXIT_FAILURE'))
+    except FileNotFoundError:
         pass
-    default_sys_excepthook(*args)
-sys.excepthook = signal_failure
-
-CTF_STAR = rsp.read_star(args.in_mics, tablefmt=rsp.TableFormat.LIST_OF_ROW_DICTS, convert_numeric=False)
-# Infer the relevant MotionCor job by using the first two parts of the micrograph path from the first entry in CTF_STAR
-# TODO This will fail if multiple MotionCor jobs are being joined. This is unlikely to occur with on-th-fly processing.
-moco_star_path = os.path.join(*explode_path(CTF_STAR['micrographs'][0]['rlnMicrographName'])[:2],
-                              'corrected_micrographs.star')
-MOCO_STAR = rsp.read_star(moco_star_path, block_list=['micrographs'], flatten=True, convert_numeric=False,
-                          tablefmt=rsp.TableFormat.LIST_OF_ROW_DICTS)
-# * Pre-exiting results already in this job directory (args.o/micrographs.star)
-output_path = os.path.join(args.o, 'micrographs.star')
-if os.path.isfile(output_path):
-    previous_output_star = rsp.read_star(output_path, block_list=['micrographs'], flatten=True, convert_numeric=False,
-                                         tablefmt=rsp.TableFormat.LIST_OF_ROW_DICTS)
-else:
-    previous_output_star = []
-
-###
-# Take advantage of the time-sorted nature of the entries to skip to the new ones
-#  * Add new rows to the previous output
-#  * Create .png previews in the Previews/ directory for the web server of:
-#   - The micrograph
-#   - The FFT/idealized CTF previews written by CTFFind
-#   - The gnuplot output from CTFFind
-if not os.path.isdir('Previews'):
-    os.mkdir('Previews')
-first_new_line = len(previous_output_star)
-to_do = queue.Queue()
-for new_row, moco_row in zip(CTF_STAR['micrographs'][first_new_line:], MOCO_STAR[first_new_line:]):
-    new_row.update(moco_row)
-    previous_output_star.append(new_row)
-    micrograph_path = new_row['rlnMicrographName']
-    #mrc2png(micrograph_path, output_dir='Previews/', resize=args.mic_png_size, sigma_contrast=args.mic_sigma_contrast)
-    to_do.put((mrc2png, micrograph_path,
-               {'output_dir': 'Previews/', 'resize': args.mic_png_size, 'sigma_contrast': args.mic_sigma_contrast}))
-    ctf_image_path = new_row['rlnCtfImage'][:-4]
-    #mrc2png(ctf_image_path, output_dir='Previews/', resize=args.fft_png_size)
-    to_do.put((mrc2png, ctf_image_path,
-               {'output_dir': 'Previews/', 'resize': args.fft_png_size}))
-    ctf_avrot_path = ctf_image_path[:-4] + '_avrot.txt'
-    #ctf2png(ctf_avrot_path, output_dir='Previews/', size=args.ctf_png_size)
-    to_do.put((ctf2png, ctf_avrot_path, {'output_dir': 'Previews/', 'size': args.ctf_png_size}))
-
-for _ in range(args.j):
-    t = threading.Thread(target=worker, args=[to_do])
-    t.start()
-to_do.join()
 
 
-###
-# Write a new micrographs.star, preserving the data_optics table too
-with open(output_path, 'w') as fh:
-    rsp.write_table(fh, CTF_STAR['optics'], 'optics', inputfmt=rsp.TableFormat.LIST_OF_ROW_DICTS)
-    rsp.write_table(fh, previous_output_star, 'micrographs', inputfmt=rsp.TableFormat.LIST_OF_ROW_DICTS)
+def main():
+    ###
+    # Relion will call this script like:
+    #     $0 --o External/jobXXX/ --in_YYY ZZZ --LABELN VALUEN --j J
+    # and the current working directory will be the root of the Relion project at hand
+    parser = argparse.ArgumentParser(description="Consolidate Relion MotionCorr and CtfFind jobs and generate preview "
+                                                 "images for use with the mvf web display",
+                                     epilog="https://github.com/fullerjamesr/mvf")
+    parser.add_argument("--o", required=True)
+    parser.add_argument("--in_mics", required=True)
+    parser.add_argument("--j", type=int, default=1)
+    parser.add_argument("--mic_png_size", type=int, default=1448)
+    parser.add_argument("--fft_png_size", type=int, default=0)
+    parser.add_argument("--ctf_png_size", type=int, default=0)
+    parser.add_argument("--mic_sigma_contrast", type=float, default=2.0)
+    args = parser.parse_args()
 
-###
-# Write out a .star file that will make the micrographs.star output usable in the Relion GUI as input to future jobs
-with open(os.path.join(args.o, 'RELION_OUTPUT_NODES.star'), 'w') as fh:
-    contents = OrderedDict((('rlnPipeLineNodeName', [output_path]), ('rlnPipeLineNodeType', [1])))
-    rsp.write_table(fh, contents, block_name='output_nodes')
+    # Engage the scaffolding that will touch the appropriate filenames to indicate success or failure to Relion
+    atexit.register(normal_exit, args.o)
+    sys.excepthook = signal_failure_factory(args.o)
+    # ...and remove any old status indicator files
+    clear_prior_exits(args.o)
 
-###
-# Write out hints to the frontend as a simple file listing the output dir and micrograph count
-with open('.mvf_progress_hint', 'w') as fh:
-    fh.write(output_path)
-    fh.write(" ")
-    fh.write(str(len(previous_output_star)))
-    fh.write("\n")
+    ctf_star = rsp.read_star(args.in_mics, tablefmt=rsp.TableFormat.LIST_OF_ROW_DICTS, convert_numeric=False)
+    # no point in running if there's nothing to process
+    if 'micrographs' not in ctf_star or len(ctf_star['micrographs'] == 0):
+        return
 
-###
-# Relion knows a job is complete when the program touches a file names RELION_JOB_EXIT_SUCCESS in the job directory
-# Uncaught exceptions that terminate this script early are dealt with by the sys.excepthook stuff above
-with open(RELION_JOB_EXIT_SUCCESS, 'w') as fh:
-    pass
+    # TODO: Huge assumption past this point: that any input to this job is a superset of the rows already processed,
+    #   that the two are sorted the same, all come from the same single MotionCorr job, and nothing needs to change with
+    #   the optics groups.
+
+    output_path = os.path.join(args.o, 'micrographs.star')
+    if os.path.isfile(output_path):
+        previous_output_mics = rsp.read_star(output_path, block_list=['micrographs'], flatten=True,
+                                             convert_numeric=False,
+                                             tablefmt=rsp.TableFormat.LIST_OF_ROW_DICTS)
+    else:
+        previous_output_mics = []
+
+    # ...or if there's nothing new
+    if len(previous_output_mics) == len(ctf_star['micrographs']):
+        return
+
+    # rlnMicrographName records will be like:
+    #     MotionCorr/jobXXX/arbitrary/raw/data/organization/file.mrc
+    # Need to extract the first two path chunks to locate the MotionCorr output directory
+    moco_star_path = os.path.join(*explode_path(ctf_star['micrographs'][0]['rlnMicrographName'])[:2],
+                                  'corrected_micrographs.star')
+    moco_star = rsp.read_star(moco_star_path, block_list=['micrographs'], flatten=True, convert_numeric=False,
+                              tablefmt=rsp.TableFormat.LIST_OF_ROW_DICTS)
+
+    # Take advantage of the time-sorted nature of the entries to skip to the new ones, do the following:
+    #  * Add new rows to the previous output
+    #  * For new rows, create .png previews in the Previews/ directory for the web server of:
+    #   - The micrograph
+    #   - The FFT/idealized CTF previews written by CTFFind
+    #   - The gnuplot output from CTFFind
+    if not os.path.isdir('Previews'):
+        os.mkdir('Previews')
+    first_new_line = len(previous_output_mics)
+    to_do = queue.Queue()
+    for new_row, moco_row in zip(ctf_star['micrographs'][first_new_line:], moco_star[first_new_line:]):
+        new_row.update(moco_row)
+        previous_output_mics.append(new_row)
+
+        micrograph_path = new_row['rlnMicrographName']
+        ctf_fft_path = new_row['rlnCtfImage'][:-4]
+        ctf_avrot_path = ctf_fft_path[:-4] + '_avrot.txt'
+
+        # mrc2png(micrograph_path, output_dir='Previews/',
+        #         resize=args.mic_png_size, sigma_contrast=args.mic_sigma_contrast)
+        to_do.put((mrc2png, micrograph_path,
+                   {'output_dir': 'Previews/', 'resize': args.mic_png_size, 'sigma_contrast': args.mic_sigma_contrast}))
+        # mrc2png(ctf_fft_path, output_dir='Previews/', resize=args.fft_png_size)
+        to_do.put((mrc2png, ctf_fft_path,
+                   {'output_dir': 'Previews/', 'resize': args.fft_png_size}))
+        # ctf2png(ctf_avrot_path, output_dir='Previews/', size=args.ctf_png_size)
+        to_do.put((ctf2png, ctf_avrot_path, {'output_dir': 'Previews/', 'size': args.ctf_png_size}))
+
+    for _ in range(args.j):
+        t = threading.Thread(target=threaded_worker, args=[to_do])
+        t.start()
+    to_do.join()
+
+    # Write a new micrographs.star, preserving the data_optics table too
+    with open(output_path, 'w') as fh:
+        rsp.write_table(fh, ctf_star['optics'], block_name='optics', inputfmt=rsp.TableFormat.LIST_OF_ROW_DICTS)
+        rsp.write_table(fh, previous_output_mics, block_name='micrographs', inputfmt=rsp.TableFormat.LIST_OF_ROW_DICTS)
+
+    # Write out a .star file that will make the micrographs.star output usable in the Relion GUI as input to future jobs
+    with open(os.path.join(args.o, 'RELION_OUTPUT_NODES.star'), 'w') as fh:
+        contents = OrderedDict((('rlnPipeLineNodeName', [output_path]), ('rlnPipeLineNodeType', [1])))
+        rsp.write_table(fh, contents, block_name='output_nodes')
+
+    # Write out hints to the mvf app frontend as a simple file listing the output dir and micrograph count
+    with open('.mvf_progress_hint', 'w') as fh:
+        fh.write(output_path)
+        fh.write(" ")
+        fh.write(str(len(previous_output_mics)))
+        fh.write("\n")
+
+
+if __name__ == '__main__':
+    main()
